161d5c4125dfbf9e185582aa636b2a5d32c04f59
William Schmae <williamschmae@Williams-MacBook-Pro.local>
2023-03-08 13:18:53 -0700

    5.18

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -8,7 +8,7 @@ describe('Blog app', function() {
     }
     cy.request('POST', 'http://localhost:3003/api/users/', user) 
     cy.visit('http://localhost:3000');
-  })
+  });
 
   it('Login form is shown', function() {
     cy.contains('login');
@@ -17,4 +17,31 @@ describe('Blog app', function() {
   it('Login form can be opened', function() {
     cy.contains('login').click();
   })
+});
+
+describe('Login', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'testUser1',
+      username: 'testUser1',
+      password: 'password'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user) 
+    cy.visit('http://localhost:3000');
+    cy.contains('login').click()
+  });
+
+  it('succeeds with correct credentials', function() {
+    cy.get('#username').type('testUser1');
+    cy.get('#password').type('password');
+    cy.get('#login-button').click()
+  });
+
+  it('fails with incorrect credentials', function() {
+    cy.get('#username').type('fakeuser');
+    cy.get('#password').type('fakePassword');
+    cy.get('#login-button').click()
+    cy.contains('Invalid username or password')
+  });
 });
